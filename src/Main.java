686d8ccf3fb4b8e81dc072ff7736f037d79fc915
Begiims <attokurovabegimaj@gmail.com>
2023-01-26 18:56:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = {56,234,657,78,45};
+
+        Class classes = new Class(45,"Messi",array);
+        System.out.println(classes);
+    }
+}
+
